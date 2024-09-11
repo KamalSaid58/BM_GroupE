038b86cd77b4f4be7cb1a8a639470e20f685e084
Kamal Said <kamal.said58@gmail.com>
2024-09-12 01:22:58 +0300

    update jar

--- src/main/java/com/transfer/controller/LogoutController.java
+++ src/main/java/com/transfer/controller/LogoutController.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class LogoutController {
 
